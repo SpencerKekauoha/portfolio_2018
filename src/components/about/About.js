8943f1b5fe17b474403896da553870b2129a4778
SpencerKekauoha <spkekauoha@gmail.com>
2018-06-30 15:30:07 -0600

    updated contact page with new design

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,6 @@ class About extends React.Component {
     super();
 
     this.state = {
-      description: "I'm a Frontend Web Developer based in Salt Lake City, Utah.  I love the challenge of coding and figuring out best practices of development and learning new tech. If you are interested in my work, feel free to contact me.",
       linkedin: 'https://www.linkedin.com/in/spencerkekauoha/',
       email: 'mailto:spkekauoha@gmail.com',
       github: 'https://github.com/SpencerKekauoha'
@@ -19,16 +18,23 @@ class About extends React.Component {
 
     return (
       <div className="about">
-        <div className="about-card">
-          <div className="about-information">
-            <img src={profileImage} alt=""/>
-            <div className="container">
-              <h3>Hi, I'm Spencer.</h3>
-              <p>{this.state.description}</p>
-              <div className="contact-info">
-                <h5>Contact me on <a href={this.state.linkedin} target="_blank" rel="noopener noreferrer">Linkedin</a></h5>
-                <h5>View my <a href={this.state.github} target="_blank" rel="noopener noreferrer">Github</a></h5>
-                <h5>Or send me an <a href={this.state.email}>email</a></h5>
+        <div className="about-header">
+          <img src={profileImage} alt=""/>
+        </div>
+        <div className="about-header-message">
+          <h3>Hi, I'm Spencer.</h3>
+        </div>
+        <div className="about-card-container">
+          <div className="about-card">
+            <div className="about-information">
+
+              <div className="container">
+                <p>I'm a Frontend Web Developer based in Salt Lake City, Utah.  I love the challenge of coding and figuring out best practices of development and learning new tech. If you are interested in my work, feel free to contact me.</p>
+                <div className="contact-info">
+                  <h5>Contact me on <a href={this.state.linkedin} target="_blank" rel="noopener noreferrer">Linkedin</a></h5>
+                  <h5>View my <a href={this.state.github} target="_blank" rel="noopener noreferrer">Github</a></h5>
+                  <h5>Or send me an <a href={this.state.email}>email</a></h5>
+                </div>
               </div>
             </div>
           </div>
